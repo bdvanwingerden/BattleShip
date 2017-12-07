cee9cebb7102209ac92c67df96d6f3a8b8b0954a
doriangh3 <doriangh3@gmail.com>
2017-12-07 12:45:06 -0500

    Cliennnnnnntblah
    n/a

--- src/client/BattleClient.java
+++ src/client/BattleClient.java
@@ -3,25 +3,48 @@ package client;
 import common.MessageListener;
 import common.MessageSource;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.Socket;
 
 public class BattleClient extends MessageSource implements MessageListener {
+
+    /**Address of battleship server*/
     private InetAddress host;
+
+    /**Port to make available*/
     private int port;
+
+    /**User name of client*/
     private String userName;
+
+    private Socket clientSocket;
+
+    ConnectionAgent connectA;
+
 
     public BattleClient(InetAddress host, int port, String userName) {
         this.host = host;
         this.port = port;
         this.userName = userName;
+
+        try {
+            clientSocket = new Socket(host, port);
+        }catch(IOException ioe){
+            System.err.println("Couldn't get I/O for the connection to the host "
+                    + host);
+        }
+
+        connectA = new ConnectionAgent(clientSocket);
     }
 
     public void connect(){
 
-    }
+
+    }//end connect()
 
     public void messageReceived(String message, MessageSource source){
-
+        //will print out message received
     }
 
     public void sourceClosed(MessageSource source){
--- src/client/BattleDriver.java
+++ src/client/BattleDriver.java
@@ -1,4 +1,43 @@
 package client;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class BattleDriver {
-}
+
+    /**
+     * Main method: This is the entry point for the clients
+     * @param args
+     */
+    public static void main(String[] args){
+
+        /**Defaualt port number*/
+        int port = 5700;
+
+        /**Address of our battleship server*/
+        InetAddress host;
+
+        /**User name of client*/
+        String username;
+
+        //command line args are Hostname, Port, and username
+        if(args.length != 3){
+            System.out.println("Usage: <protocol> [<host name>] [<port>] [<username>]");
+            System.exit(1); //1 -> Wrong number of arguments
+        }
+        else{
+            try {
+                port = Integer.parseInt(args[2]);
+                host = InetAddress.getByName(args[0]);
+                //####Create new Client here!!!####
+            }catch(UnknownHostException e){
+                System.out.println("UnknownHostException: " + e.getMessage());
+            }
+        }
+
+
+
+
+    }//end main
+
+}//end class BattleDriver
