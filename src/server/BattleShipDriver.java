3e3415c4031f2371cfb3302a37a86232ada2ea0d
bdvanwingerden <bdvanwingerden1@catamount.wcu.edu>
2017-12-08 16:58:04 -0500

    DONE

--- src/server/BattleShipDriver.java
+++ src/server/BattleShipDriver.java
@@ -6,7 +6,15 @@ public class BattleShipDriver {
     static final int PORT = 5700;
 
     public static void main(String args[]){
-        BattleServer bs = new BattleServer(PORT);
+
+        BattleServer bs;
+
+        if(args.length > 0){
+            bs = new BattleServer(Integer.parseInt(args[0]));
+        }else{
+            bs = new BattleServer(PORT);
+        }
+
 
         try {
             bs.listen();
